2e9e5c14165f0edb91bde5ddc8646987fde128ef
Michael <mikeishere3@gmail.com>
2018-10-30 13:28:41 -0400

    two unit tests made

---
+++ ThirtyDaysOfTDD.UnitTests/StringUtils.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThirtyDaysOfTDD.UnitTests
+{
+    class StringUtils
+    {
+        internal int FindNumberOfOccurences(string sentenceToScan, string characterToScanFor)
+        {
+            var stringToCheckAsCharacterArray = sentenceToScan.ToCharArray();
+            var characterToCheckFor = Char.Parse(characterToScanFor);
+            var numberOfOccurences = 0;
+
+            for (var charIdx = 0; charIdx < stringToCheckAsCharacterArray.GetUpperBound(0); charIdx++)
+            {
+                if (stringToCheckAsCharacterArray[charIdx] == characterToCheckFor)
+                {
+                    numberOfOccurences++;
+                }
+            }
+
+            return numberOfOccurences;
+        }
+    }
+}
---
+++ ThirtyDaysOfTDD.UnitTests/StringUtilsTest.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThirtyDaysOfTDD.UnitTests
+{
+    [TestFixture]
+    public class StringUtilsTest
+    {
+        [Test]
+        public void ShouldBeAbleToCountNumberOfLettersInSimpleSentence()
+        {
+            var sentenceToScan = "TDD is awesome!";
+            var characterToScanFor = "e";
+            var expectedResult = 2;
+            var stringUtils = new StringUtils();
+
+            int result = stringUtils.FindNumberOfOccurences(sentenceToScan, characterToScanFor);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        public void ShouldBeAbleToCountNumberOfLettersInAComplexSentence()
+        {
+            var sentenceToScan = "Once is unique, twice is a coincidence, three times is a pattern.";
+            var characterToScanFor = "n";
+            var expectedResult = 5;
+            var stringUtils = new StringUtils();
+
+            int result = stringUtils.FindNumberOfOccurences(sentenceToScan, characterToScanFor);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
